6ed82f976fcc568824fa147e09121a80c5fc19cf
TiagoAlcan <tiago.gomesalcan@gmail.com>
2024-03-06 20:42:41 -0300

    adicionando unidades de medida

--- Orgs/app/src/main/java/br/com/fiap/orgs/MainActivity.kt
+++ Orgs/app/src/main/java/br/com/fiap/orgs/MainActivity.kt
@@ -1,8 +1,13 @@
 package br.com.fiap.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
-    
 }
